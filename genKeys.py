e337607cf067b30aee8f329841cc07a6e8db7f80
Eric Ye <computereric@live.ca>
2013-01-26 17:14:24 -0500

    Initial commit

---
+++ genKeys.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv, random, sys
+
+numStudents = 73
+
+students = []
+
+student_emails_file = open("Class.csv")
+student_emails = csv.reader(student_emails_file)
+next(student_emails) #get rid of the header row
+# Remember: each row is in 'First Name', 'Last name', 'email' format
+
+
+for i in student_emails:
+    #make a list of tuples between firstnames, emails and bits
+    students.append((i[0], i[2], random.getrandbits(128)))
+
+outfile = open('keys.secret', 'w')
+student_emails_file.close()
+writer = csv.writer(outfile)
+writer.writerows(students)
+outfile.close()
---
+++ sendEmails.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys, smtplib, csv
+from email.mime.text import MIMEText
+
+fromaddr = 'topsnight2013@gmail.com'
+username = 'topsnight2013@gmail.com'
+password = input('Enter the password for %s: ' % username)
+
+keysFile = open('keys.secret')
+
+msgtemplate = """
+Hi {firstname},
+
+Please vote at [the amazingly stylish] {url} for TOPSNight leadership.
+
+Use your email and secure pin as you see below:
+email: {email}
+pin: {hashedpin}
+"""
+
+##server = smtplib.SMTP('smtp.gmail.com:587')
+##server.starttls()
+##server.login(username, password)
+
+def sendSingleEmail(keyFileRow, server):
+    hashedpin = int(keyFileRow[2])
+    toEmail = keyFileRow[1]
+    msg = MIMEText(msgtemplate.format(firstname=keyFileRow[0], url='',
+                             hashedpin=hashedpin, email=toEmail))
+    msg['Subject'] = 'TOPSNight leadership voting'
+    msg['From'] = fromaddr
+    msg['To'] = toEmail
+    print(msg)
+    
+##    try:
+##        server.send_message(msg)
+##    except:
+##        print("Message was not sent to %s" % toEmail, file=sys.stderr)
+
+keys = csv.reader(keysFile)
+
+for keyRow in keys:
+    sendSingleEmail(keyRow, None)
+
+##server.quit()
