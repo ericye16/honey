5dd5e015f7d5e48823f770693e4dd3134846a21e
Eric Ye <computereric@live.ca>
2013-01-27 02:03:35 -0500

    Fixed things; now more data!

---
+++ readKeys.py
@@ -0,0 +1,22 @@
+from google.appengine.ext.remote_api import remote_api_stub
+import honey
+import getpass
+import csv
+
+def auth_func():
+    return (raw_input('Username:'), getpass.getpass('Password:'))
+
+remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', auth_func,
+                                   'localhost:8080')
+
+##code
+keysFile = open('keys.secret')
+keysCSV = csv.reader(keysFile)
+for keyRow in keysCSV:
+    print keyRow
+    person = honey.Person(
+        email = keyRow[1],
+        key_ = int(keyRow[2]) - 9223372036854775807)
+    person.put()
+
+keysFile.close()
